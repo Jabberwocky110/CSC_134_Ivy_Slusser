8ceb36e85b5094764686c4a338b323c18e95bea5
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2026-06-12 17:18:55 -0400

    Save

---
+++ .vscode/DividingByZero.cpp
@@ -0,0 +1,18 @@
+/*
+@author: Ivy Slusser
+@date: 06/10/2026
+@purpose: This program determines a number divided by zero.
+*/
+//Include the iostream library for input and output
+#include <iostream>
+using namespace std;
+//A function is created.
+int main ()
+{
+    //There is an integer.
+    int num1 = 9;
+    //The quotient is calculated.
+    int quotient = num1/0;
+    cout << "The quotient of " << num1 << "and " << 0 << "is " << quotient;
+    return 0;
+}
