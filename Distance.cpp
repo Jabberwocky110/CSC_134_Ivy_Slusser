e3f083b9040cd3289d28dbb43b4e70c4f69faab0
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2026-06-12 20:49:52 -0400

    Distance

---
+++ Distance.cpp
@@ -0,0 +1,17 @@
+/*
+@author: Ivy Slusser
+@date: 06/10/2026
+@purpose: This program calculates distance.
+*/
+//Include the iostream library for input and output
+#include <iostream>
+//Use namespace to avoid typing std
+using namespace std;
+int main()
+{
+   int v = 80;
+   int t = 2;
+   int x = v * t;
+   cout << "The distance traveled is " << x << " miles.";
+   return 0;
+}
