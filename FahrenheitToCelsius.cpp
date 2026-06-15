90ca55f05f61d842ebfa6510a63de39d94818acc
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2026-06-14 21:58:25 -0400

    Fahrenheit

---
+++ FahrenheitToCelsius.cpp
@@ -0,0 +1,14 @@
+/*
+@author: Ivy Slusser
+@date: 06/14/2026
+@purpose: This program converts temperature from Fahrenheit to Celsius.
+*/
+//Include the iostream library for input and output
+#include <iostream>
+//Use namespace to avoid typing std
+using namespace std;
+//A function is created.
+int main ()
+{
+    
+}
