4669c573e749c9e25c259254c9e431a65a349efc
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2026-06-14 21:08:31 -0400

    Hi!

---
+++ Hi.cpp
@@ -0,0 +1,20 @@
+/*
+@author: Ivy Slusser
+@date: 06/14/2026
+@purpose: This program prints "Hi" as a character string.
+*/
+//Include the iostream library for input and output
+#include <iostream>
+//Use namespace to avoid typing std
+using namespace std;
+//A function is created.
+int main ()
+{
+    //Characters are created.
+    char H = 'H';
+    char i = 'i';
+    char x = '!';
+    //The word Hi! is printed.
+    cout << H << i << x;
+    return 0;
+}
