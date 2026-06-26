21522e596e563a977270c692a23de5649a5ac08d
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2026-06-26 12:48:22 -0400

    Comment

--- TenGrades.cpp
+++ TenGrades.cpp
@@ -10,16 +10,25 @@ using namespace std;
 //A function is created.
 int main ()
 {
+   //A counter is created for the loop.
    int GradeCount = 1;
+   //The score or grades added starts at zero.
    float score = 0;
+   //A variable is created to hold the grades.
    float Grade;
+   //The loop iterates ten times.
    while (GradeCount <= 10)
    {
+      //The user can see how many grades they entered.
       cout << "Enter grade " << GradeCount << " \n";
+      //The user enters a grade.
       cin >> Grade;
+      //The counter for the loop increases.
       GradeCount++;
+      //The new grade is added to the score.
       score += Grade;
    }
+   //The average is calculated.
    float Average = score/10;
    cout << "Your average grade is " << Average;
    return 0;
