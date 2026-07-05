1dc466d9a8f61cfb8ea582b0c5db2a015b64abd2
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2026-07-05 19:29:57 -0400

    Table

--- MultiplicationTable.cpp
+++ MultiplicationTable.cpp
@@ -10,13 +10,17 @@ using namespace std;
 //A function is created.
 int main()
 {
+   //The user inputs a number.
    int number;
    cout << "Enter a number: ";
    cin >> number;
+   //This is the title for the table.
    cout << "Number | Number Multiplied" << endl;
    cout << "---------------------------" << endl;
+   //The loop iterates from 0 to 12.
    for (int count = 0; count <= 12; count++)
    {
+      //The number and its result are printed.
       cout << number << " " << number * count << endl;
    }
 }
