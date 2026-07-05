144164e4e0d9234ec4aed8fb36dac14b66bbcba8
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2026-07-04 21:23:27 -0400

    Index

--- ForLoop0-10.cpp
+++ ForLoop0-10.cpp
@@ -1,7 +1,7 @@
 /*
 @author: Ivy Slusser
 @date: 07/04/2026
-@purpose: This program generates a while loop that iterates from 0 to 10.
+@purpose: This program generates a for loop that iterates from 0 to 10.
 */
 //Include the iostream library for input and output
 #include <iostream>
---
+++ ForLoopIndexIterations.cpp
@@ -0,0 +1,14 @@
+/*
+@author: Ivy Slusser
+@date: 07/04/2026
+@purpose: This program generates a for loop that iterates the number of times a user selects.
+*/
+//Include the iostream library for input and output
+#include <iostream>
+//Use namespace to avoid typing std
+using namespace std;
+//A function is created.
+int main()
+{
+
+}
