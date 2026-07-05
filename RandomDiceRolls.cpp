6fa8c3561e16cfc16f19b24c6428de662e29b4cb
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2026-07-05 19:44:36 -0400

    Dice

--- RandomDiceRolls.cpp
+++ RandomDiceRolls.cpp
@@ -5,10 +5,27 @@
 */
 //Include the iostream library for input and output
 #include <iostream>
+//This is needed for randomness
+#include <cstdlib>
+//This is needed for time
+#include <ctime>
 //Use namespace to avoid typing std
 using namespace std;
 //A function is created.
 int main()
 {
-
+    //The user inputs how many times they want to roll the die.
+   int RollNumber;
+   cout << "Enter how many times you want to roll the die ";
+   cin >> RollNumber;
+   //This seeds the random number generator.
+   srand(static_cast<unsigned int>(time(0)));
+   //The loop iterates the number of times the user determined.
+   for (int count = 1; count <= RollNumber; count++)
+   {
+     //The dice number is generated.
+     int DiceNumber = rand() % 6 + 1;
+     //The user sees what they rolled.
+     cout << "You rolled " << DiceNumber << endl;
+   }
 }
