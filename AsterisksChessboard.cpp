c79f3cc6faec7e51114f70c9a5d6b8a04b85e8a1
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2026-07-07 14:40:55 -0400

    Start

---
+++ AsterisksChessboard.cpp
@@ -0,0 +1,14 @@
+/*
+@author: Ivy Slusser
+@date: 07/07/2026
+@purpose: This program generates a chessboard pattern of asterisks with the row length determined by the user.
+*/
+//Include the iostream library for input and output
+#include <iostream>
+//Use namespace to avoid typing std
+using namespace std;
+//A function is created.
+int main()
+{
+   
+}
