2749de9429733071c16b4dc2c4fc406d687a3c89
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2026-07-08 13:51:10 -0400

    Budget

---
+++ Budget.cpp
@@ -0,0 +1,15 @@
+/*
+@author: Ivy Slusser
+@date: 07/08/2026
+@purpose: This program determines whether a user is successfully budgeting.
+*/
+//Include the iostream library for input and output
+#include <iostream>
+//Use namespace to avoid typing std
+using namespace std;
+//A function is created.
+int main()
+{
+   double amount;
+   cout << "Enter the amount of money in your budget: ";
+}
