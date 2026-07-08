129e427248a3073f5bf28a0658328308a3909781
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2026-07-08 13:46:46 -0400

    Fee

--- PinehurstCountryClub.cpp
+++ PinehurstCountryClub.cpp
@@ -10,9 +10,16 @@ using namespace std;
 //A function is created.
 int main()
 {
+   //The cost of the country club membership is listed here.
    double fee = 250000;
+   //The current cost is printed.
+   cout << "The current sales are " << fee << endl;
+   //The loop iterates 5 times.
    for (int count = 0; count < 5; count++)
    {
-
+       //Each year the fee increases by six percent.
+       fee = fee * 1.06;
+       //The fee is printed for each projected year.
+       cout << "The projected sales for year " << count + 1 << " are: " << fee << endl;
    }
 }
