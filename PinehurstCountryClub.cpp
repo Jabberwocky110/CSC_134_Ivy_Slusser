5e0a2a869b3eca9eaf81f675dd619a57ecf62ec3
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2026-07-08 13:34:52 -0400

    Count

---
+++ PinehurstCountryClub.cpp
@@ -0,0 +1,18 @@
+/*
+@author: Ivy Slusser
+@date: 07/08/2026
+@purpose: This program calculates an increase in membership fees for the Pinehurst Country Club.
+*/
+//Include the iostream library for input and output
+#include <iostream>
+//Use namespace to avoid typing std
+using namespace std;
+//A function is created.
+int main()
+{
+   double fee = 250000;
+   for (int count = 0; count < 5; count++)
+   {
+
+   }
+}
