c68ebc4f5b1bbba1e623557c5a90063970846a51
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2026-07-16 18:52:55 -0400

    Days

---
+++ DaysOfTheWeek.cpp
@@ -0,0 +1,20 @@
+/*
+@author: Ivy Slusser
+@date: 07/16/2026
+@purpose: This program lists all eight planets and Pluto, using an array.
+*/
+//Include the iostream library for input and output
+#include <iostream>
+//Use namespace to avoid typing std
+using namespace std;
+//A function is created.
+int main()
+{
+   //An array is created with all of the days of the week.
+   string WeeksArray[7] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+   //The days are printed 1-7.
+   for (int value = 1; value <= 7; value++)
+   {
+      cout << "Day " << value << ": " << WeeksArray[value - 1] << endl;
+   }
+}
