e945c509b8038cffb3138cc9f609ad1945b79801
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2026-07-16 19:42:09 -0400

    Arrays

--- ValuesOfArrayPrinted.cpp
+++ ValuesOfArrayPrinted.cpp
@@ -12,16 +12,16 @@ int main()
 {
   //The size of the array is defined.
   const int SIZE = 7;
-  //The values are listed.
+  //The values are listed, and the latter values default to zero.
   int numberList[SIZE] = {1, 2, 4, 8};
   //Each value of the array is printed based on its index.
   for (int value = 0; value < SIZE; value++)
   {
-     cout << numberList[value] << endl;
+     cout << "Array value " << value + 1 << ": " << numberList[value] << endl;
   }
   //The user is also able to select a position in the array to print.
   int Position;
-  cout << "Enter the position in the array that you would like to reference: " << endl;
+  cout << "Enter the position (1-7) in the array that you would like to reference: " << endl;
   cin >> Position;
   //The position in the array that the user selected is printed.
   cout << numberList[Position - 1] << endl;
