b8835366cc23101a0b01bae17c1a627af99418de
Jabberwocky110 <slusserip562@mail.sandhills.edu>
2026-07-17 13:50:10 -0400

    ZerosArray

--- ArraySorted.cpp
+++ ArraySorted.cpp
@@ -12,4 +12,25 @@ int main()
 {
   const int TOTAL_SIZE = 10;
   int numbers[TOTAL_SIZE] = {10, 90, 2, 25, 44, 88, 0, 4};
+  int index = 0;
+  int NumbersSorted[TOTAL_SIZE] = {};
+  for (int value = 0; value < TOTAL_SIZE; value++)
+  {
+    for (int PreviousValue = numbers[index]; PreviousValue < TOTAL_SIZE; index++)
+    {
+     if (numbers[value + 1] <= PreviousValue)
+     {
+       NumbersSorted[PreviousValue] = numbers[value + 1];
+     }
+     else
+     {
+       NumbersSorted[PreviousValue] = numbers[value];
+     }
+    }
+  }
+  cout << "The array sorted is ";
+  for  (int i = 0; i < TOTAL_SIZE; i++)
+  {
+    cout << NumbersSorted[i] << " ";
+  }
 }
